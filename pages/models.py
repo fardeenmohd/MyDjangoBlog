b6f928c2810c1af60fe935993cebf41b19a9f0b1
fardeenmohd <fardeenmohdpl@gmail.com>
2018-10-16 19:31:42 +0200

    Add model Post Add table posts to store all the posts for the blog Add admin user to manage the post table

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    id = models.SmallIntegerField(primary_key=True)
+    date = models.DateField()
+    content = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.content)
